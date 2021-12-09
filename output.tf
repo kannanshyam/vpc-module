96ce98327b810cc79a06f9608864af0e59279feb
Kannan MS <94454999+kannanshyam@users.noreply.github.com>
2021-12-09 12:38:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,27 @@
-output "vpc_id" {
-  value = aws_vpc.vpc.id
+output "vpc-id" {
+    value = aws_vpc.vpc.id
+}
+
+output "subnet-public1" {
+    value = aws_subnet.public1.id
+}
+
+output "subnet-public2" {
+    value = aws_subnet.public2.id
+}
+
+output "subnet-public3" {
+    value = aws_subnet.public3.id
+}
+
+output "subnet-private1" {
+    value = aws_subnet.private1.id
+}
+
+output "subnet-private2" {
+    value = aws_subnet.private2.id
+}
+
+output "subnet-private3" {
+    value = aws_subnet.private3.id
 }
